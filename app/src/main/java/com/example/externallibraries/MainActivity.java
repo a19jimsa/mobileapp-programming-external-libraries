6fb85af758a4fcee96c53dfe0ca686ef982a597f
a19jimsa <a19jimsa@student.his.se>
2021-04-19 20:50:49 +0200

    Added a new fragment.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.externallibraries;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -25,7 +28,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("a19jimsa", "click");
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new BlankFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new CardFragment()).setReorderingAllowed(true).commit();
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Fragment fragment = null;
+                Log.d("a19jimsa","clicked on " + item);
+                switch (item.getItemId()){
+                    case R.id.favorite:
+                        fragment = new CardFragment();
+                        break;
+                    case R.id.search:
+                        fragment = new BlankFragment();
+                        break;
+                    case R.id.settings:
+                        fragment = new SettingsFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).setReorderingAllowed(true).commit();
+                return true;
             }
         });
     }

5e6ce9275e5b3392d793969292334ae06990761e
a19jimsa <a19jimsa@student.his.se>
2021-04-19 11:22:41 +0200

    Added icons and added menu items.

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.externallibraries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -12,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MaterialButton materialButton = findViewById(R.id.materialButton);
+
     }
 }
